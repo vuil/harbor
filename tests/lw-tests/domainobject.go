64589f56e7f2e99d0c1e58b9f225ed3c72fa7965
Sagar Muchhal <muchhals@vmware.com>
2017-03-21 11:57:13 -0700

    Integration tests with Lightwave instance
    This patch introduces a Lightwave-Harbor integration test suite which will be run as
    part of the Travis CI build.
    The LW instance is a custom deployed endpoint on AWS with some
    pre-configured users.

---
+++ tests/lw-tests/domainobject.go
@@ -0,0 +1,43 @@
+package lwtest
+
+// Search is the accumalation of Project/Repository search results
+type Search struct {
+
+	// Search results of the projects that matched the filter keywords.
+	Projects []SearchProject `json:"project,omitempty"`
+
+	// Search results of the repositories that matched the filter keywords.
+	Repositories []SearchRepository `json:"repository,omitempty"`
+}
+
+// SearchProject maps search result JSON to object
+type SearchProject struct {
+
+	ID int64 `json:"id,omitempty"`
+
+	Name string `json:"name,omitempty"`
+
+	Public int32 `json:"public,omitempty"`
+}
+
+// SearchRepository maps search result JSON to object
+type SearchRepository struct {
+
+	ProjectID int32 `json:"project_id,omitempty"`
+
+	ProjectName string `json:"project_name,omitempty"`
+
+	ProjectPublic int32 `json:"project_public,omitempty"`
+
+	RepositoryName string `json:"repository_name,omitempty"`
+}
+
+// ProjectReq contains parameters for Project creation
+type ProjectReq struct {
+
+	// The name of the project.
+	ProjectName string `json:"project_name,omitempty"`
+
+	// The public status of the project.
+	Public int32 `json:"public,omitempty"`
+}
